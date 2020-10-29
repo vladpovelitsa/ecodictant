3d01e1e80f48b983deca16650cba3f13ff530bce
vladpovelitsa <vlad.povelitsa@gmail.com>
2020-10-29 13:15:33 +0200

    inner pages done

--- app/js/main.js
+++ app/js/main.js
@@ -323,3 +323,113 @@ function showHiddenSelect(target) {
   }
 
 }
+
+$('.media__slider').slick({
+  dots:false,
+  slidesToShow: 3,
+  autoplay: true,
+  autoplaySpeed: 4000,
+  responsive: [
+    {
+      breakpoint: 1250,
+      settings: {
+        slidesToShow:2,
+      }
+    },
+    {
+      breakpoint: 767,
+      settings: {
+        slidesToShow: 1,
+      }
+    },
+  ]
+})
+
+function placesInfoToggler() {
+  var e = event.target;
+  if(e.classList.contains('places__info-toggler')) {
+    e.classList.toggle('active')
+    e.nextElementSibling.classList.toggle('active')
+  }
+}
+
+document.addEventListener('click', placesInfoToggler)
+
+
+
+
+var clipPlay = false
+function findVideos() {
+  let videos = document.querySelectorAll('.clip_cover');
+
+  for (let i = 0; i < videos.length; i++) {
+    setupVideo(videos[i]);
+  }
+}
+
+function setupVideo(video) {
+  let link = video.querySelector('.video__link');
+  let media = video.querySelector('.video__media');
+  let button = video.querySelector('.video__button');
+  let id = parseMediaURL(media);
+
+  video.addEventListener('click', () => {
+    let iframe = createIframe(id);
+
+    link.remove();
+    button.remove();
+    video.appendChild(iframe);
+  });
+
+  link.removeAttribute('href');
+  video.classList.add('video--enabled');
+}
+
+function parseMediaURL(media) {
+  let regexp = /https:\/\/i\.ytimg\.com\/vi\/([a-zA-Z0-9_-]+)\/maxresdefault\.jpg/i;
+  let url = media.src;
+  let match = url.match(regexp);
+
+  return match[1];
+}
+
+function createIframe(id) {
+  let iframe = document.createElement('iframe');
+
+  iframe.setAttribute('allowfullscreen', '');
+  iframe.setAttribute('allow', 'autoplay');
+  iframe.setAttribute('src', generateURL(id));
+  iframe.classList.add('video__media');
+
+  return iframe;
+}
+
+function generateURL(id) {
+  let query = '?rel=0&showinfo=0&autoplay=1';
+
+  return 'https://www.youtube.com/embed/' + id + query;
+}
+
+findVideos();
+
+
+$('.lessons__slider').slick({
+  dots:false,
+  slidesToShow: 3,
+  autoplay: true,
+  autoplaySpeed: 4000,
+  responsive: [
+    {
+      breakpoint: 1250,
+      settings: {
+        slidesToShow:2,
+      }
+    },
+    {
+      breakpoint: 767,
+      settings: {
+        slidesToShow: 1,
+      }
+    },
+  ]
+})
